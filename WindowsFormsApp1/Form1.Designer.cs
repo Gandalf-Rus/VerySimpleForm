cdf559ac4fb5c492f39a5231b99282ffae7cfe4f
rus <rus@DESKTOP-DIIU7L5>
2022-08-31 09:58:58 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Age = new System.Windows.Forms.TextBox();
+            this.Phone = new System.Windows.Forms.TextBox();
+            this.NamePerson = new System.Windows.Forms.TextBox();
+            this.LastName = new System.Windows.Forms.TextBox();
+            this.NameLb = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Simbit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Age
+            // 
+            this.Age.Location = new System.Drawing.Point(99, 115);
+            this.Age.Name = "Age";
+            this.Age.Size = new System.Drawing.Size(117, 20);
+            this.Age.TabIndex = 1;
+            // 
+            // Phone
+            // 
+            this.Phone.Location = new System.Drawing.Point(99, 154);
+            this.Phone.Name = "Phone";
+            this.Phone.Size = new System.Drawing.Size(117, 20);
+            this.Phone.TabIndex = 2;
+            // 
+            // NamePerson
+            // 
+            this.NamePerson.Location = new System.Drawing.Point(99, 37);
+            this.NamePerson.Name = "NamePerson";
+            this.NamePerson.Size = new System.Drawing.Size(117, 20);
+            this.NamePerson.TabIndex = 3;
+            // 
+            // LastName
+            // 
+            this.LastName.Location = new System.Drawing.Point(99, 76);
+            this.LastName.Name = "LastName";
+            this.LastName.Size = new System.Drawing.Size(117, 20);
+            this.LastName.TabIndex = 4;
+            // 
+            // NameLb
+            // 
+            this.NameLb.AutoSize = true;
+            this.NameLb.Location = new System.Drawing.Point(12, 44);
+            this.NameLb.Name = "NameLb";
+            this.NameLb.Size = new System.Drawing.Size(38, 13);
+            this.NameLb.TabIndex = 5;
+            this.NameLb.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Last name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Age:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Phone Number:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 250);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NameLb);
+            this.Controls.Add(this.LastName);
+            this.Controls.Add(this.NamePerson);
+            this.Controls.Add(this.Phone);
+            this.Controls.Add(this.Age);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Age;
+        private System.Windows.Forms.TextBox Phone;
+        private System.Windows.Forms.TextBox NamePerson;
+        private System.Windows.Forms.TextBox LastName;
+        private System.Windows.Forms.Label NameLb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var db = new BloggingContext())
+            {
+                int.TryParse(Age.Text, out int age);
+
+                var person = new Person 
+                {
+                    Name = NamePerson.Text,
+                    LastName = LastName.Text,
+                    Age = age,
+                    PhoneNumber = button1.Text,
+                };
+
+                db.Persons.Add(person);
+                db.SaveChanges();
+                this.Close();
+
+                //search in DB with the help LINQ
+                //var query = from b in db.Persons
+                //            orderby b.Id
+                //            select b;
+
+                //foreach (var item in query)
+                //{
+                //    Console.WriteLine($"Id: {item.Id}\tName: {item.Name}\tLast Name: {item.LastName}\tAge: {item.Age}\tPhone Number{item.PhoneNumber}");
+                //}
+            }
+        }
+    }
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+    }
+}
